78b8c2da7c3c184349c714ca916d601fda16ba0c
Eric <eric@bostrot.com>
2023-03-15 00:52:50 +0100

    Hide empty results

--- Main.cs
+++ Main.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation
+// Copyright (c) Microsoft Corporation
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -189,6 +189,12 @@ namespace Community.PowerToys.Run.Plugin.Winget
                         catch (Exception e)
                         {
                             name = e.ToString();
+                        }
+
+                        // Check if result is empty
+                        if (name == string.Empty)
+                        {
+                            continue;
                         }
 
                         string title = $"{name} ({idStr})";
