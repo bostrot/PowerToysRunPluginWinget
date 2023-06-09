30f99b8e46f5f6312ab7e3ac0ff11d5fd590a0a7
Eric <eric@bostrot.com>
2023-06-09 22:01:28 +0200

    Added installer

---
+++ installer.ps1
@@ -0,0 +1,25 @@
+# PowerShell script that downloads latest release from GitHub and installs it to PowerToys Run plugin folder
+# Usage: .\installer.ps1
+
+$installLocation = "C:\Program Files\PowerToys\modules\launcher\Plugins\Winget"
+
+# Get latest release from GitHub
+$latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/bostrot/PowerToysRunPluginWinget/releases/latest"
+
+# Get download URL for latest release
+$downloadUrl = $latestRelease.assets.browser_download_url
+
+# Download latest release
+Invoke-WebRequest -Uri $downloadUrl -OutFile "Winget.tmp.zip"
+
+# Unzip latest release
+Expand-Archive -Path "Winget.tmp.zip" -DestinationPath "$installLocation.tmp"
+
+# Move files to plugin folder
+Move-Item "$installLocation.tmp\Winget" $installLocation
+
+# Remove zip file
+Remove-Item "Winget.tmp.zip"
+
+# Remove temporary folder
+Remove-Item "$installLocation.tmp" -Recurse
