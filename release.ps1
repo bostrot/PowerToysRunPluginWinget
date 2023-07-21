6f9c2b83f1d46f32a748d58118b5bdd139079f18
Eric <eric@bostrot.com>
2023-07-21 20:21:46 +0200

    Cleaned up imports and version bump

--- release.ps1
+++ release.ps1
@@ -1,6 +1,6 @@
 # Script for copying the release files and creating the release zip file
 # C:\Users\erict\Sync\Projects\PowerToys\x64\Release\modules\launcher\Plugins\Winget
-$version = "1.2.1"
+$version = "1.2.2"
 $release = "C:\Users\erict\Downloads\winget-powertoys-$version.zip"
 $zip = "C:\Program Files\7-Zip\7z.exe"
 $path = "C:\Users\erict\Sync\Projects\PowerToys\x64\Release\modules\launcher\Plugins\Winget"
