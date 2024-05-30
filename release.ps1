0c02f6424d58174021a2a8bc8230313e66ed277e
Eric <eric@bostrot.com>
2024-05-30 20:29:38 +0200

    Version bump to 1.3.0

--- release.ps1
+++ release.ps1
@@ -1,9 +1,9 @@
 # Script for copying the release files and creating the release zip file
 # C:\Users\erict\PowerToys-dev\x64\Release\RunPlugins\Winget
-$version = "1.2.3"
+$version = "1.3.0"
 $release = "C:\Users\erict\Downloads\winget-powertoys-$version.zip"
 $zip = "C:\Program Files\7-Zip\7z.exe"
-$path = "C:\Users\erict\PowerToys-dev\x64\Release\RunPlugins\Winget"
+$path = "D:\erict\PowerToys-dev\x64\Release\RunPlugins\Winget"
 
 # pack the files from path and excluding
 &$zip a -aoa -bb0 -bso0 -xr!PowerToys* -xr!Backup* -xr!Ijwhost* -tzip $release $path
