2f678c4970ad87f10ad12e7508c8951e9d18f975
devCKVargas <vargaschrisking@gmail.com>
2024-06-12 11:36:42 +0800

    feat(7zip): add a check if it exists, if not install it using winget

--- release.ps1
+++ release.ps1
@@ -3,6 +3,9 @@
 $version = "1.3.0"
 $release = "$env:USERPROFILE\Downloads\winget-powertoys-$version.zip"
 $zip = "$env:ProgramFiles\7-Zip\7z.exe"
+if (-not (Test-Path $zip)) {
+	winget install 7zip.7zip -h --disable-interactivity --accept-source-agreements --accept-package-agreements
+}
 $path = "D:\erict\PowerToys-dev\x64\Release\RunPlugins\Winget"
 
 # pack the files from path and excluding
